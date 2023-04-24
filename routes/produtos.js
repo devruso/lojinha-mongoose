e466c6cf6e6fe892093c1d76905569e6b0fcd13e
devruso <jamilsonjr@live.com>
2023-04-24 15:41:05 -0300

    put inserido

--- routes/produtos.js
+++ routes/produtos.js
@@ -21,8 +21,19 @@ try{
     }
 }catch(err){
     console.log(err);
-    res.status(500).json({message:"Um erro ocorreu"});  
+    res.status(500).json({message:"Um erro ocorreu"});
+    
 }
 })
 
+router.put("/produtos/:id", async (req,res) =>{
+    const {id} = req.params;
+    const {nome, descricao, quantidade,preco, desconto, dataDesconto, categoria} = req.body;
+    const attProduto = await Produto.findByIdAndUpdate(id,{nome, descricao, quantidade,preco, desconto, dataDesconto, categoria});
+    if(attProduto){
+        res.json({message:"Produto atualizado"});
+    }else{
+        res.status(400).json({message:"Informacoes inválidas"});
+    }
+})
 module.exports = router;
